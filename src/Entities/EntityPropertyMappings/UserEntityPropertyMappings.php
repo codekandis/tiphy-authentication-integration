211c7d71d1fc43524e661397d9664f94acfcbab7
Christian Ramelow <info@codekandis.net>
2021-04-18 00:32:43 +0200

    Adds the class `UserEntityPropertyMappings`.

---
+++ src/Entities/EntityPropertyMappings/UserEntityPropertyMappings.php
@@ -0,0 +1,25 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphyAuthenticationIntegration\Entities\EntityPropertyMappings;
+
+use CodeKandis\Tiphy\Converters\TwoWaysConverters\StringToBoolConverter;
+use CodeKandis\Tiphy\Entities\EntityPropertyMappings\EntityPropertyMapping;
+use CodeKandis\Tiphy\Entities\EntityPropertyMappings\EntityPropertyMappings;
+
+/**
+ * Represents the entity property mappings of a user entity.
+ * @package codekandis/tiphy-authentication-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class UserEntityPropertyMappings extends EntityPropertyMappings
+{
+	/**
+	 * Constructor method.
+	 */
+	public function __construct()
+	{
+		$this->add(
+			new EntityPropertyMapping( 'id', null ),
+			new EntityPropertyMapping( 'isActive', new StringToBoolConverter() ),
+		);
+	}
+}
