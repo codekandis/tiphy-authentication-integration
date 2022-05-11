692d1a9f2564709e3776e1eea3f2eaef6f3264d7
Christian Ramelow <info@codekandis.net>
2022-05-12 00:21:53 +0200

    Adds the session authenticator configuration.

---
+++ src/Configurations/SessionAuthenticatorConfiguration.php
@@ -0,0 +1,21 @@
+<?php declare( strict_types = 1 );
+namespace CodeKandis\TiphyAuthenticationIntegration\Configurations;
+
+use CodeKandis\Authentication\Configurations\SessionAuthenticatorConfigurationInterface;
+use CodeKandis\Configurations\AbstractConfiguration;
+
+/**
+ * Represents a session authenticator configuration.
+ * @package codekandis/tiphy-authentication-integration
+ * @author Christian Ramelow <info@codekandis.net>
+ */
+class SessionAuthenticatorConfiguration extends AbstractConfiguration implements SessionAuthenticatorConfigurationInterface
+{
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getRegisteredClientSessionKey(): string
+	{
+		return $this->read( 'registeredClientSessionKey' );
+	}
+}
