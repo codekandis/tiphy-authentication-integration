735375808de199b06a44a140819bcd312ec3c9e1
Christian Ramelow <info@codekandis.net>
2022-08-11 00:42:25 +0200

    Fixes the imports.

--- src/Persistence/MariaDb/Repositories/Authentication/UsersRepository.php
+++ src/Persistence/MariaDb/Repositories/Authentication/UsersRepository.php
@@ -2,10 +2,18 @@
 namespace CodeKandis\TiphyAuthenticationIntegration\Persistence\MariaDb\Repositories\Authentication;
 
 use CodeKandis\Entities\EntityPropertyMappings\EntityPropertyMapper;
+use CodeKandis\Persistence\FetchingResultFailedException;
 use CodeKandis\Persistence\Repositories\AbstractRepository;
+use CodeKandis\Persistence\SettingFetchModeFailedException;
+use CodeKandis\Persistence\StatementExecutionFailedException;
+use CodeKandis\Persistence\StatementPreparationFailedException;
+use CodeKandis\Persistence\TransactionCommitFailedException;
+use CodeKandis\Persistence\TransactionRollbackFailedException;
+use CodeKandis\Persistence\TransactionStartFailedException;
 use CodeKandis\TiphyAuthenticationIntegration\Entities\EntityPropertyMappings\UserEntityPropertyMappings;
 use CodeKandis\TiphyAuthenticationIntegration\Entities\UserEntity;
 use CodeKandis\TiphyAuthenticationIntegration\Entities\UserEntityInterface;
+use ReflectionException;
 
 /**
  * Represents a repository for any user entity.
@@ -16,6 +24,14 @@ class UsersRepository extends AbstractRepository implements UsersRepositoryInter
 {
 	/**
 	 * {@inheritDoc}
+	 * @throws ReflectionException The entity class to reflect does not exist.
+	 * @throws TransactionStartFailedException The transaction failed to start.
+	 * @throws TransactionRollbackFailedException The transaction failed to roll back.
+	 * @throws TransactionCommitFailedException The transaction failed to commit.
+	 * @throws StatementPreparationFailedException The preparation of the statement failed.
+	 * @throws StatementExecutionFailedException The execution of the statement failed.
+	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
+	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
 	public function readUserByKey( UserEntityInterface $user ): ?UserEntityInterface
 	{
--- src/Persistence/MariaDb/Repositories/Authentication/UsersRepositoryInterface.php
+++ src/Persistence/MariaDb/Repositories/Authentication/UsersRepositoryInterface.php
@@ -1,15 +1,7 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\TiphyAuthenticationIntegration\Persistence\MariaDb\Repositories\Authentication;
 
-use CodeKandis\Persistence\MariaDb\FetchingResultFailedException;
-use CodeKandis\Persistence\MariaDb\SettingFetchModeFailedException;
-use CodeKandis\Persistence\MariaDb\StatementExecutionFailedException;
-use CodeKandis\Persistence\MariaDb\StatementPreparationFailedException;
-use CodeKandis\Persistence\MariaDb\TransactionCommitFailedException;
-use CodeKandis\Persistence\MariaDb\TransactionRollbackFailedException;
-use CodeKandis\Persistence\MariaDb\TransactionStartFailedException;
 use CodeKandis\TiphyAuthenticationIntegration\Entities\UserEntityInterface;
-use ReflectionException;
 
 /**
  * Represents the interface of any users respository of any user entity.
@@ -22,14 +14,6 @@ interface UsersRepositoryInterface
 	 * Reads a user specified by its ID.
 	 * @param UserEntityInterface $user The ID of the user.
 	 * @return ?UserEntityInterface The user if it exists, otherwise null.
-	 * @throws ReflectionException The entity class to reflect does not exist.
-	 * @throws TransactionStartFailedException The transaction failed to start.
-	 * @throws TransactionRollbackFailedException The transaction failed to roll back.
-	 * @throws TransactionCommitFailedException The transaction failed to commit.
-	 * @throws StatementPreparationFailedException The preparation of the statement failed.
-	 * @throws StatementExecutionFailedException The execution of the statement failed.
-	 * @throws SettingFetchModeFailedException The setting of the fetch mode of the statement failed.
-	 * @throws FetchingResultFailedException The fetching of the statment result failed.
 	 */
 	public function readUserByKey( UserEntityInterface $user ): ?UserEntityInterface;
 }
